0156c551436dd29b9d7fb77251352a36e4b125a2
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-11-21 04:32:19 +0900

    💄 place time input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@
 
 import { People } from '@/components/People';
 import { PresetButtonList } from '@/components/PresetButtonList';
+import { TimeInput } from '@/components/TimeInput';
 import { usePresets } from '@/hooks/usePresets';
+import { useTime } from '@/hooks/useTime';
 
 export default function Home() {
   const presets = usePresets([]);
+  const time = useTime(0, 0);
   return (
     <div className="container mx-auto px-4">
       <div className="flex gap-3 max-w-lg mx-auto">
@@ -16,6 +19,7 @@ export default function Home() {
           <People presets={presets} onAdd={() => presets.addPerson(presets.currentPreset)} />
         </div>
       </div>
+      <TimeInput {...time} />
     </div>
   );
 }
---
+++ src/hooks/useTime.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+type useTimeType = {
+  hour: number;
+  minute: number;
+  onHourChange: (hour: number) => void;
+  onMinuteChange: (minute: number) => void;
+};
+export const useTime = (initialHour: number, initialMinute: number): useTimeType => {
+  const [hour, setHour] = useState<number>(initialHour);
+  const [minute, setMinute] = useState<number>(initialMinute);
+  return {
+    hour,
+    minute,
+    onHourChange: setHour,
+    onMinuteChange: setMinute,
+  };
+};
